3e030e44f034cf5ec2ce64102908d7b7161df69b
Baraka Mulumia <65751779+Baraka-Mulumia@users.noreply.github.com>
2024-05-23 21:44:54 +0300

    Create sessions-execution-config.schema.ts

---
+++ src/sessions/schema/sessions-execution-config.schema.ts
@@ -0,0 +1,26 @@
+import * as mongoose from 'mongoose';
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({
+  timestamps: true,
+  versionKey: false,
+  collection: 'sessions_execution_config',
+})
+export class SessionsExecutionConfig extends mongoose.Document {
+  @Prop({ required: true })
+  last_execution_id: number;
+
+  @Prop({ required: true })
+  last_execution_date: Date;
+
+  @Prop({ required: true })
+  execution_interval: number;
+
+  @Prop({ required: false, default: 3 })
+  executions_per_interval: number;
+}
+
+export const SessionsExecutionConfigSchema = SchemaFactory.createForClass(
+  SessionsExecutionConfig,
+);
